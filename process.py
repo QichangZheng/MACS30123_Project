2ef45d05f5c23cac5b97abf702528513ef540112
QichangZheng <qichangzheng@uchciago.edu>
2023-05-25 20:03:18 -0500

    1

--- process.py
+++ process.py
@@ -7,8 +7,8 @@ comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-filename = 'dataset_zh_train'
-df = pd.read_json(filename + '.json', lines=True)[:100]
+filename = 'dataset_zh_test'
+df = pd.read_json(filename + '.json', lines=True)[:10]
 num = len(df) / size
 df = df.iloc[round(rank * num): round((rank + 1) * num)]
 
@@ -39,12 +39,12 @@ def get_score(text):
 
 df['scores'] = df['review_body'].apply(get_score)
 print(df['scores'].tolist())
-# all_data = comm.gather(df['scores'].tolist(), root=0)
+all_data = comm.gather(df['scores'].tolist(), root=0)
 #
-# if rank == 0:
-#     all_scores = [score for sublist in all_data for score in sublist]
-#     df = pd.read_json(filename + '.json', lines=True)[:100]
-#     result_df = pd.DataFrame(all_scores, columns=['score1', 'score2', 'score3', 'score4', 'score5', 'score6'])
-#     df = pd.concat([df, result_df], axis=1)
-#     df.to_csv('processed_' + filename + '.csv', index=False)
-#     print('Done!')
+if rank == 0:
+    all_scores = [score for sublist in all_data for score in sublist]
+    df = pd.read_json(filename + '.json', lines=True)[:100]
+    result_df = pd.DataFrame(all_scores, columns=['score1', 'score2', 'score3', 'score4', 'score5', 'score6'])
+    df = pd.concat([df, result_df], axis=1)
+    df.to_csv('processed_' + filename + '.csv', index=False)
+    print('Done!')
